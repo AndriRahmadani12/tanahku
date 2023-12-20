e9425a26ca632353a534414288858c0edd19c2dc
Andri Rahmadani <62165852+AndriRahmadani12@users.noreply.github.com>
2023-12-20 11:02:40 +0700

    Delete Cloud Computing directory

--- Cloud Computing/getdata-tanahku-main/Routes/router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router =express.Router();
-
-const control = require('../controller/control');
-
-router.get('/tanaman', control.getAllTanaman)
-router.get('/tanaman/:id_tanaman', control.getIdTanaman)
-
-module.exports = router;
